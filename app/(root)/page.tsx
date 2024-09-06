231950258d944677dbbc020e411c845f210f8c45
Kessiah U Providence <Kessiahp91@gmail.com>
2024-09-06 11:41:16 -0400

    'initalize total balance box'

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
-const Home
- = () => {
+const Home = () => {
+  const loggedIn = { firstName: 'Kessiah'};
+
   return (
-    <div> Home
-    </div>
-  )
-}
+    <section className="home">
+      
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and transactions efficiently."
+          />
 
-export default Home
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={2310.67}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
