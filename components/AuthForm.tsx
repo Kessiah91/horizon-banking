b79a41883a8d8f6bf01ff631997071939dd8edb4
Kessiah U Providence <Kessiahp91@gmail.com>
2024-10-16 15:25:28 -0400

    corrected name issue, but still popping up, will investigate more

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,7 +21,7 @@ import { Loader2 } from "lucide-react";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { useRouter } from "next/navigation";
-import { signUp } from "@/lib/actions/user.actions";
+import { getLoggedInUser, signUp } from "@/lib/actions/user.actions";
 import { signIn } from "@/lib/actions/user.actions";
 
 // Update schema to include password validation
@@ -33,9 +33,11 @@ import { signIn } from "@/lib/actions/user.actions";
 // });
 
 const AuthForm = ({ type }: { type: string }) => {
-  const router = useRouter
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setisLoading] = useState(false);
+ 
+  
 
   const formSchema = authFormSchema(type);
 
@@ -61,12 +63,12 @@ const AuthForm = ({ type }: { type: string }) => {
       }
 
       if (type === "sign-in") {
-        // const response = await signIn({
-        //   email: data.email,
-        //   password: data.password,
-        // })
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        })
 
-        // if(response) router.push('/')
+        if(response) router.push('/')
       }
     } catch (error) {
       console.log(error);
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,9 +5,13 @@ import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
 
-export const signIn = async () => {
+export const signIn = async ({ email, password }: signInProps ) => {
      try {
+      const { account } = await createAdminClient();
 
+      const response = await account.createEmailPasswordSession(email, password);
+
+      return parseStringify(response);
      } catch (error) {
         console.error('Error', error);
      }
@@ -26,9 +30,9 @@ export const signUp = async (userData: SignUpParams) => {
       // Set the session cookie
       cookies().set("appwrite-session", session.secret, {
         path: "/",
+        httpOnly: true,
         sameSite: "strict",
         secure: true,
-        httpOnly: true,
       });
 
       return parseStringify(newUserAccount);
@@ -40,8 +44,24 @@ export const signUp = async (userData: SignUpParams) => {
 export async function getLoggedInUser() {
    try {
       const { account } = await createSessionClient();
-      return await account.get();
+      
+      const user =  await account.get();
+
+      return parseStringify(user);
    } catch (error) {
+      console.log(error)
       return null;   
+   }
+}
+
+export const  logoutAccount = async () => {
+   try {
+         const { account } = await createSessionClient();
+
+         cookies().delete('appwrite-session');
+
+         await account.deleteSession('current'); 
+   } catch (error) {
+      return null;
    }
 }
